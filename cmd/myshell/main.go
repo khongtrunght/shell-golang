7e7dbf07793a4b2fd32d04cadbe74819305f7587
khongtrunght <khongtrunght@gmail.com>
2024-06-13 17:51:53 +0700

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -14,18 +14,43 @@ type Shell struct {
 	writer io.Writer
 }
 
+func (s *Shell) Repl() {
+	for {
+		s.Run()
+	}
+}
+
 func (s *Shell) Run() {
 	fmt.Fprint(s.writer, "$ ")
-
+	var command string
 	// Wait for user input
-	commandName, err := bufio.NewReader(s.reader).ReadString('\n')
-	commandName = strings.TrimSpace(commandName)
+	userInput, err := bufio.NewReader(s.reader).ReadString('\n')
+	userInput = strings.TrimSpace(userInput)
 	if err != nil {
 		log.Println("Error reading input: ", err)
 	}
 
+	splits := strings.Split(userInput, " ")
+	if len(splits) == 0 {
+		return
+	} else {
+		command = splits[0]
+	}
+
+	if command == "exit" {
+		if len(splits) == 2 {
+			if splits[1] == "0" {
+				os.Exit(0)
+			} else {
+				os.Exit(1)
+			}
+		} else {
+			fmt.Fprintln(s.writer, "exit: invalid argument")
+		}
+	}
+
 	// <command_name>: command not found
-	fmt.Fprintf(s.writer, "%s: command not found\n", commandName)
+	fmt.Fprintf(s.writer, "%s: command not found\n", userInput)
 }
 
 func main() {
@@ -34,7 +59,5 @@ func main() {
 		writer: os.Stdout,
 	}
 
-	for {
-		shell.Run()
-	}
+	shell.Repl()
 }
