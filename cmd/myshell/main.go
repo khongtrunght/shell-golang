363da33bb6109107d14fcf4b183e368ecd65ec95
khongtrunght <khongtrunght@gmail.com>
2024-06-13 17:46:31 +0700

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -3,21 +3,38 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
 )
 
-func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+type Shell struct {
+	reader io.Reader
+	writer io.Writer
+}
+
+func (s *Shell) Run() {
+	fmt.Fprint(s.writer, "$ ")
 
 	// Wait for user input
-	commandName, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	commandName, err := bufio.NewReader(s.reader).ReadString('\n')
 	commandName = strings.TrimSpace(commandName)
 	if err != nil {
 		log.Println("Error reading input: ", err)
 	}
 
 	// <command_name>: command not found
-	fmt.Fprintf(os.Stdout, "%s: command not found\n", commandName)
+	fmt.Fprintf(s.writer, "%s: command not found\n", commandName)
+}
+
+func main() {
+	shell := &Shell{
+		reader: os.Stdin,
+		writer: os.Stdout,
+	}
+
+	for {
+		shell.Run()
+	}
 }
