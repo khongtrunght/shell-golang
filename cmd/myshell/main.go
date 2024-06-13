8d85486df231c001c2a5eee84b98ac41db1140ef
khongtrunght <khongtrunght@gmail.com>
2024-06-13 17:56:50 +0700

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -20,6 +20,33 @@ func (s *Shell) Repl() {
 	}
 }
 
+type Command func(args []string, writer io.Writer)
+
+func exitCommand(args []string, writer io.Writer) {
+	if len(args) == 1 {
+		if args[1] == "0" {
+			os.Exit(0)
+		} else {
+			os.Exit(1)
+		}
+	} else {
+		fmt.Fprintln(writer, "exit: invalid argument")
+	}
+}
+
+func echoCommand(args []string, writer io.Writer) {
+	if len(args) == 1 {
+		fmt.Fprintln(writer, "")
+	} else {
+		fmt.Fprintln(writer, strings.Join(args[1:], " "))
+	}
+}
+
+var commands = map[string]Command{
+	"exit": exitCommand,
+	"echo": echoCommand,
+}
+
 func (s *Shell) Run() {
 	fmt.Fprint(s.writer, "$ ")
 	var command string
@@ -37,16 +64,9 @@ func (s *Shell) Run() {
 		command = splits[0]
 	}
 
-	if command == "exit" {
-		if len(splits) == 2 {
-			if splits[1] == "0" {
-				os.Exit(0)
-			} else {
-				os.Exit(1)
-			}
-		} else {
-			fmt.Fprintln(s.writer, "exit: invalid argument")
-		}
+	if cmd, ok := commands[command]; ok {
+		cmd(splits[1:], s.writer)
+		return
 	}
 
 	// <command_name>: command not found
